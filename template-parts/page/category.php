72cbdf65f935db9d44cdcc0a2c825af3265dd9b2
unknown <richardslaura24@gmail.com>
2021-08-24 09:31:20 +0800

    inital commit

---
+++ template-parts/page/category.php
@@ -0,0 +1,133 @@
+<?php
+/**
+ * @package WordPress
+ * @subpackage
+ * @since
+ * @version 1.0
+ */
+
+?>
+	
+
+<?php get_header(); ?>
+
+<main>
+
+    <div class="grid-x grid-margin-x">
+      <div class="medium-5 large-8 cell">
+      <div class="row column text-center">
+        <h2 class="subheader">Title</h2>
+      </div>
+    </div>
+    <div class="large-4 cell">
+      <div class="filter">
+    <div class="cell">
+      <label>Category
+          <select>
+            <option value="husker">Husker</option>
+            <option value="starbuck">Starbuck</option>
+            <option value="hotdog">Hot Dog</option>
+            <option value="apollo">Apollo</option>
+          </select>
+        </label>
+  </div>
+</div>
+</div>
+    </div>
+
+<div class="bottom-container">
+  <div class="grid-x align-bottom">
+    <div class="medium-2 cell">
+      <div class="media-object">
+        <div class="media-object-section">
+        </div>
+         
+        </div>
+      </div>
+
+
+    </div>
+    <div class="medium-2 cell">
+      <div class="media-object">
+        <div class="media-object-section">
+          <img class="thumbnail" src="img/placeholder.png">
+        </div>
+        <div class="media-object-section">
+          <h5>Post Title</h5>
+    <p>Lorem ipsum dolor sit amet, consectetur
+      adipiscing elit. Lorem ipsum dolor sit amet, consectetur
+      adipiscing elit. Lorem ipsum dolor sit amet, consectetur
+      adipiscing elit.</p>
+      <a href="single-blog-page.html">Read More</a>
+        </div>
+      </div>
+
+    </div>
+    <div class="medium-2 cell">
+      <div class="media-object">
+        <div class="media-object-section">
+          <img class="thumbnail" src="img/placeholder.png">
+        </div>
+        <div class="media-object-section">
+          <h5>Post Title</h5>
+    <p>Lorem ipsum dolor sit amet, consectetur
+      adipiscing elit. Lorem ipsum dolor sit amet, consectetur
+      adipiscing elit. Lorem ipsum dolor sit amet, consectetur
+      adipiscing elit.</p>
+      <a href="single-blog-page.html">Read More</a>
+        </div>
+      </div>
+
+    </div>
+    <div class="medium-2 cell">
+      <div class="media-object">
+        <div class="media-object-section">
+          <img class="thumbnail" src="img/placeholder.png">
+        </div>
+        <div class="media-object-section">
+          <h5>Post Title</h5>
+    <p>Lorem ipsum dolor sit amet, consectetur
+      adipiscing elit. Lorem ipsum dolor sit amet, consectetur
+      adipiscing elit. Lorem ipsum dolor sit amet, consectetur
+      adipiscing elit.</p>
+      <a href="single-blog-page.html">Read More</a>
+        </div>
+      </div>
+      
+    </div>
+    <div class="medium-2 cell">
+      <div class="media-object">
+        <div class="media-object-section">
+          <img class="thumbnail" src="img/placeholder.png">
+        </div>
+        <div class="media-object-section">
+          <h5>Post Title</h5>
+    <p>Lorem ipsum dolor sit amet, consectetur
+      adipiscing elit. Lorem ipsum dolor sit amet, consectetur
+      adipiscing elit. Lorem ipsum dolor sit amet, consectetur
+      adipiscing elit.</p>
+      <a href="single-blog-page.html">Read More</a>
+        </div>
+      </div>
+
+    </div>
+    <div class="medium-2 cell">
+
+      <div class="media-object">
+        <div class="media-object-section">
+          <img class="thumbnail" src="img/placeholder.png">
+        </div>
+        <div class="media-object-section">
+          <h5>Post Title</h5>
+    <p>Lorem ipsum dolor sit amet, consectetur
+      adipiscing elit. Lorem ipsum dolor sit amet, consectetur
+      adipiscing elit. Lorem ipsum dolor sit amet, consectetur
+      adipiscing elit.</p>
+      <a href="single-blog-page.html">Read More</a>
+        </div>
+      </div>
+
+    </div>
+  </div>
+</div>
+</main>
