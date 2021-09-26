b53effea978859a6c41edf27f74447c20433434a
Katrous <45927461+Katrous@users.noreply.github.com>
2021-09-26 21:59:03 +0800

    blog posts dynamically load on all pages

--- front-page.php
+++ front-page.php
@@ -50,122 +50,44 @@
         </div>
       </div>
       <br>
+
       <div class="main-bottom">
-      <div class="grid-x align-bottom">
+
+        <?php
+        // Define our WP Query Parameters
+        $the_query = new WP_Query( 'posts_per_page=3' ); ?>
+          
+          
+          <div class="grid-x align-top">
+        <?php 
+        // Start our WP Query
+        while ($the_query -> have_posts()) : $the_query -> the_post(); 
+        // Display the Post Title with Hyperlink
+        ?>
+
           <div class="medium-4 cell">
             <div class="media-object">
               <div class="media-object-section">
                 <img class="thumbnail" src="<?php echo get_theme_file_uri("/img/placeholder.png")?>">
               </div>
               <div class="media-object-section">
-                <h5>Post Title</h5>
-          <p>Lorem ipsum dolor sit amet, consectetur
-            adipiscing elit.</p>
-            <a href="#">Read More</a>
-              </div>
-            </div>
-            <div class="media-object">
-              <div class="media-object-section">
-                <img class="thumbnail" src="<?php echo get_theme_file_uri("/img/placeholder.png")?>">
-              </div>
-              <div class="media-object-section">
-                <h5>Post Title</h5>
-          <p>Lorem ipsum dolor sit amet, consectetur
-            adipiscing elit.</p>
-            <a href="#">Read More</a>
-              </div>
-            </div>
-            <div class="media-object">
-              <div class="media-object-section">
-                <img class="thumbnail" src="<?php echo get_theme_file_uri("/img/placeholder.png")?>">
-              </div>
-              <div class="media-object-section">
-                <h5>Post Title</h5>
-          <p>Lorem ipsum dolor sit amet, consectetur
-            adipiscing elit.</p>
-            <a href="#">Read More</a>
-              </div>
+                <h5><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h5>
+
+                <?php 
+                // Display the Post Excerpt
+                the_excerpt(); ?>
+              </div> 
             </div>
           </div>
-          <div class="medium-4 cell">
-            <div class="media-object">
-              <div class="media-object-section">
-                <img class="thumbnail" src="<?php echo get_theme_file_uri("/img/placeholder.png")?>">
-              </div>
-              <div class="media-object-section">
-                <h5>Post Title</h5>
-          <p>Lorem ipsum dolor sit amet, consectetur
-            adipiscing elit.</p>
-            <a href="#">Read More</a>
-              </div>
-            </div>
-            <div class="media-object">
-              <div class="media-object-section">
-                <img class="thumbnail" src="<?php echo get_theme_file_uri("/img/placeholder.png")?>">
-              </div>
-              <div class="media-object-section">
-                <h5>Post Title</h5>
-          <p>Lorem ipsum dolor sit amet, consectetur
-            adipiscing elit.</p>
-            <a href="#">Read More</a>
-              </div>
-            </div>
-            <div class="media-object">
-              <div class="media-object-section">
-                <img class="thumbnail" src="<?php echo get_theme_file_uri("/img/placeholder.png")?>">
-              </div>
-              <div class="media-object-section">
-                <h5>Post Title</h5>
-          <p>Lorem ipsum dolor sit amet, consectetur
-            adipiscing elit.</p>
-            <a href="#">Read More</a>
-              </div>
-            </div>
-          </div>
-          <div class="medium-4 cell">
-            <div class="media-object">
-              <div class="media-object-section">
-                <img class="thumbnail" src="<?php echo get_theme_file_uri("/img/placeholder.png")?>">
-              </div>
-              <div class="media-object-section">
-                <h5>Post Title</h5>
-          <p>Lorem ipsum dolor sit amet, consectetur
-            adipiscing elit.</p>
-            <a href="#">Read More</a>
-              </div>
-            </div>
-            <div class="media-object">
-              <div class="media-object-section">
-                <img class="thumbnail" src="<?php echo get_theme_file_uri("/img/placeholder.png")?>">
-              </div>
-              <div class="media-object-section">
-                <h5>Post Title</h5>
-          <p>Lorem ipsum dolor sit amet, consectetur
-            adipiscing elit.</p>
-            <a href="#">Read More</a>
-              </div>
-            </div>
-            <div class="media-object">
-              <div class="media-object-section">
-                <img class="thumbnail" src="<?php echo get_theme_file_uri("/img/placeholder.png")?>">
-              </div>
-              <div class="media-object-section">
-                <h5>Post Title</h5>
-          <p>Lorem ipsum dolor sit amet, consectetur
-            adipiscing elit.</p>
-            <a href="#">Read More</a>
-              </div>
-            </div>
-          </div>
-        </div>      
+
+        <?php 
+        // Repeat the process and reset once it hits the limit
+        endwhile;
+        wp_reset_postdata();
+        ?>
+
       </div>
       <br>
-    </main>
-
-  <?php 
-
-
-
-?>
+  </main>
 
 <?php get_footer(); ?>
--- page.php
+++ page.php
@@ -72,5 +72,39 @@ while(have_posts()){
 
     <h5 class="excerpt-post-title">Make Sure To Check Out the Latest Post!</h5>
 
+    <?php
+  
+   // Define our WP Query Parameters
+   $the_query = new WP_Query( 'posts_per_page=3' ); ?>
+     
+     
+     <div class="grid-x align-top">
+   <?php 
+   // Start our WP Query
+   while ($the_query -> have_posts()) : $the_query -> the_post(); 
+   // Display the Post Title with Hyperlink
+   ?>
+
+     <div class="medium-4 cell">
+       <div class="media-object">
+         <div class="media-object-section">
+           <img class="thumbnail" src="<?php echo get_theme_file_uri("/img/placeholder.png")?>">
+         </div>
+         <div class="media-object-section">
+           <h5><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h5>
+
+           <?php 
+           // Display the Post Excerpt
+           the_excerpt(); ?>
+         </div> 
+       </div>
+     </div>
+
+   <?php 
+   // Repeat the process and reset once it hits the limit
+   endwhile;
+   wp_reset_postdata();
+   ?>
+    </div>
 <!-- INLUDE CONTACT PAGE HERE -->
 <?php get_footer(); ?>
