efdb5c72bf54f38eb1371e214d48a4ebd421effe
Katrous <45927461+Katrous@users.noreply.github.com>
2021-10-05 15:47:37 +0800

    clean file

--- page.php
+++ page.php
@@ -1,29 +1,23 @@
-<?php
-/**
- * @package WordPress
- * @subpackage
- * @since
- * @version 1.0
- */
+<?php 
+//ABOUT PAGE
+get_header();
 
-?>
-	
-
-<?php get_header(); ?>
-
-<main>
-  <br>
-      <div class="row title">
+while(have_posts()){
+  the_post(); ?>
+    <!-- title -->
+ <div class="row title">
     <div class="columns">
-      <h2>About us</h2>
+    <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
     </div>
   </div>
+
+  <hr>
+
   <!-- content -->
- 
   <div class="main-middle">
     <div class="grid-x grid-margin-x">
           <div class="medium-6 cell small-order-2 medium-order-1">
-            <img class="thumbnail" src="/wordpress/wp-content/themes/lego/assets/img/placeholder.png">
+            <img class="thumbnail" src="<?php echo get_theme_file_uri("/img/placeholder.png")?>">
           </div>
           <div class="medium-6 cell small-order-1 medium-order-2">
           <p>Lorem ipsum dolor sit amet, consectetur
@@ -67,12 +61,15 @@
                 </p>
               </div>
               <div class="medium-6 cell small-order-1 medium-order-2">
-                <img class="thumbnail" src="/wordpress/wp-content/themes/lego/assets/img/placeholder.png">
+                <img class="thumbnail" src="<?php echo get_theme_file_uri("/img/placeholder.png")?>">
               </div>
             </div>
           </div>
           <br>
+  <?php }
+?>
 
+<<<<<<< HEAD:template-parts/page/about-page.php
           <h5 class="excerpt-post-title">Make Sure To Check Out the Latest Post!</h5>
 <!--posts three-->
 <div class="bottom-container">
@@ -117,8 +114,44 @@
               adipiscing elit.<a href="home-2/single/">Read More</a></p>
                 </div>
               </div>
+=======
+
+    <h5 class="excerpt-post-title">Make Sure To Check Out the Latest Post!</h5>
+
+    <?php
+>>>>>>> Laura:page.php
   
-            </div>
-          </div>
-        </div>
-    </main>
+   // Define our WP Query Parameters
+   $the_query = new WP_Query( 'posts_per_page=3' ); ?>
+     
+     
+     <div class="grid-x bottom-posts align-top">
+   <?php 
+   // Start our WP Query
+   while ($the_query -> have_posts()) : $the_query -> the_post(); 
+   // Display the Post Title with Hyperlink
+   ?>
+
+     <div class="medium-4 cell">
+       <div class="media-object">
+         <div class="media-object-section">
+           <img class="thumbnail" src="<?php echo get_theme_file_uri("/img/placeholder.png")?>">
+         </div>
+         <div class="media-object-section">
+           <h5><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h5>
+
+           <?php 
+           // Display the Post Excerpt
+           the_excerpt(); ?>
+         </div> 
+       </div>
+     </div>
+
+   <?php 
+   // Repeat the process and reset once it hits the limit
+   endwhile;
+   wp_reset_postdata();
+   ?>
+    </div>
+<!-- INLUDE CONTACT PAGE HERE -->
+<?php get_footer(); ?>
--- single.php
+++ single.php
@@ -1,32 +1,57 @@
+<!-- THIS PAGE POWERS SINGLE BLOG PAGES -->
+
 <?php 
-/**
- * The template for displaying all single posts and attachments
- */
+get_header();
 
-get_header(); ?>
-			
-<div class="content">
+while(have_posts()){
+  the_post(); ?>
+  <div class="blog-text">
+  <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+  <br>
+  <div>
+  <img class="blog-image" src="<?php echo get_theme_file_uri("/img/placeholder.png")?>">
+</div>
+<br>
+  <?php the_content(); ?>
+  
+  <hr>
+</div>
 
-	<div class="inner-content grid-x grid-margin-x grid-padding-x">
+  <?php }
 
-		<main class="main small-12 medium-8 large-8 cell" role="main">
-		
-		    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-		
-		    	<?php get_template_part( 'parts/loop', 'single' ); ?>
-		    	
-		    <?php endwhile; else : ?>
-		
-		   		<?php get_template_part( 'parts/content', 'missing' ); ?>
+?>
+   <?php
+        // Define our WP Query Parameters
+        $the_query = new WP_Query( 'posts_per_page=3' ); ?>
+          
+          
+          <div class="grid-x bottom-posts align-top">
+        <?php 
+        // Start our WP Query
+        while ($the_query -> have_posts()) : $the_query -> the_post(); 
+        // Display the Post Title with Hyperlink
+        ?>
 
-		    <?php endif; ?>
+          <div class="medium-4 cell">
+            <div class="media-object">
+              <div class="media-object-section">
+                <img class="thumbnail" src="<?php echo get_theme_file_uri("/img/placeholder.png")?>">
+              </div>
+              <div class="media-object-section">
+                <h5><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h5>
 
-		</main> <!-- end #main -->
+                <?php 
+                // Display the Post Excerpt
+                the_excerpt(); ?>
+              </div> 
+            </div>
+          </div>
 
-		<?php get_sidebar(); ?>
+        <?php 
+        // Repeat the process and reset once it hits the limit
+        endwhile;
+        wp_reset_postdata();
+        ?>
 
-	</div> <!-- end #inner-content -->
-
-</div> <!-- end #content -->
-
+      </div>
 <?php get_footer(); ?>
