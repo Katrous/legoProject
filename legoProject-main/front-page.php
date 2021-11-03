9ba566a483270207c60751e87fe66036a970e8a8
Katrous <45927461+Katrous@users.noreply.github.com>
2021-11-03 15:51:04 +0800

    3/11 Code Update Design Updates.

---
+++ legoProject-main/front-page.php
@@ -0,0 +1,80 @@
+<?php get_header(); ?>
+
+    <main>
+      <div class="main-top">
+        <div class="grid-x grid-margin-x">
+          <div class="small-6 cell">
+              <h1>Brickin' It</h1>
+              <p>Hello, Im Adrian, I am a LEGO lover. I love collecting Lego as my favourite hobby. I have built a lot of Lego stuff using all the colourful blocks that is available everywhere. </p>
+          </div>
+          <div class="small-6 cell">
+            <img src="<?php echo get_theme_file_uri("/img/placeholder.png")?>" alt="">
+          </div>
+        </div>
+      </div>
+      
+      <br>
+      <div class="hero-section">
+        <div class="hero-section-text">
+          <p>Clickable Link For App Once Available</p>
+        </div>
+      </div>
+    <br>
+    <div class="main-middle">
+    <div class="grid-x grid-margin-x">
+          <div class="medium-6 cell small-order-2 medium-order-1">
+            <img class="thumbnail" src="<?php echo get_theme_file_uri("/img/placeholder.png")?>">
+          </div>
+          <div class="medium-6 cell small-order-1 medium-order-2">
+            <h1>Post Title</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur
+            adipiscing elit. Donec molestie, ante et
+            luctus vehicula, nisl leo porta lacus, nec
+            laoreet nisi quam a dui. </p>
+            <a href="#">Read More</a>
+            </p>
+          </div>
+        </div>
+      </div>
+      <br>
+
+      <div class="main-bottom">
+
+        <?php
+        // Define our WP Query Parameters
+        $the_query = new WP_Query( 'posts_per_page=3' ); ?>
+          
+          
+          <div class="grid-x align-top">
+        <?php 
+        // Start our WP Query
+        while ($the_query -> have_posts()) : $the_query -> the_post(); 
+        // Display the Post Title with Hyperlink
+        ?>
+
+          <div class="medium-4 cell">
+            <div class="media-object">
+              <div class="media-object-section">
+                <img class="thumbnail" src="<?php echo get_theme_file_uri("/img/placeholder.png")?>">
+              </div>
+              <div class="media-object-section">
+                <h5><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h5>
+
+                <?php 
+                // Display the Post Excerpt
+                the_excerpt(); ?>
+              </div> 
+            </div>
+          </div>
+
+        <?php 
+        // Repeat the process and reset once it hits the limit
+        endwhile;
+        wp_reset_postdata();
+        ?>
+
+      </div>
+      <br>
+  </main>
+
+<?php get_footer(); ?>
---
+++ legoProject-main/page.php
@@ -0,0 +1,176 @@
+<?php
+if(isset($_POST['user-submit'])){
+  $name = htmlspecialchars(stripslashes(trim($_POST['user-name'])));
+  $subject = "Lego Blog Email";
+  $email = htmlspecialchars(stripslashes(trim($_POST['user-email'])));
+  $message = htmlspecialchars(stripslashes(trim($_POST['user-message'])));
+  if(!preg_match("/^[A-Za-z .'-]+$/", $name)){
+    $name_error = 'Invalid name';
+  }
+  if(!preg_match("/^[A-Za-z .'-]+$/", $subject)){
+    $subject_error = 'Invalid subject';
+  }
+  if(!preg_match("/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/", $email)){
+    $email_error = 'Invalid email';
+  }
+  if(strlen($message) === 0){
+    $message_error = 'Your message should not be empty';
+  }
+}
+
+
+//ABOUT PAGE
+get_header();
+
+while(have_posts()){
+  the_post(); ?>
+    <!-- title -->
+ <div class="row title">
+    <div class="columns">
+    <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+    </div>
+  </div>
+
+  <hr>
+
+  <!-- content -->
+  <div class="main-middle">
+    <div class="grid-x grid-margin-x">
+          <div class="medium-6 cell small-order-2 medium-order-1">
+            <img class="thumbnail" src="<?php echo get_theme_file_uri("/img/placeholder.png")?>">
+          </div>
+          <div class="medium-6 cell small-order-1 medium-order-2">
+          <p>Lorem ipsum dolor sit amet, consectetur
+            adipiscing elit. Donec molestie, ante et
+            luctus vehicula, nisl leo porta lacus, nec
+            laoreet nisi quam a dui.Lorem ipsum dolor sit amet, consectetur
+            adipiscing elit. Donec molestie, ante et
+            luctus vehicula, nisl leo porta lacus, nec
+            laoreet nisi quam a dui. 
+
+            <p>Lorem ipsum dolor sit amet, consectetur
+            adipiscing elit. Donec molestie, ante et
+            luctus vehicula, nisl leo porta lacus, nec
+            laoreet nisi quam a dui. Lorem ipsum dolor sit amet, consectetur
+            adipiscing elit. Donec molestie, ante et
+            luctus vehicula, nisl leo porta lacus, nec
+            laoreet nisi quam a dui.  </p>
+            </p>
+          </div>
+        </div>
+      </div>
+      <br>
+      <div class="main-middle">
+        <div class="grid-x grid-margin-x">
+              <div class="medium-6 cell small-order-2 medium-order-1">
+                <p>Lorem ipsum dolor sit amet, consectetur
+                adipiscing elit. Donec molestie, ante et
+                luctus vehicula, nisl leo porta lacus, nec
+                laoreet nisi quam a dui.Lorem ipsum dolor sit amet, consectetur
+                adipiscing elit. Donec molestie, ante et
+                luctus vehicula, nisl leo porta lacus, nec
+                laoreet nisi quam a dui. 
+    
+                <p>Lorem ipsum dolor sit amet, consectetur
+                adipiscing elit. Donec molestie, ante et
+                luctus vehicula, nisl leo porta lacus, nec
+                laoreet nisi quam a dui. Lorem ipsum dolor sit amet, consectetur
+                adipiscing elit. Donec molestie, ante et
+                luctus vehicula, nisl leo porta lacus, nec
+                laoreet nisi quam a dui.  </p>
+                </p>
+              </div>
+              <div class="medium-6 cell small-order-1 medium-order-2">
+                <img class="thumbnail" src="<?php echo get_theme_file_uri("/img/placeholder.png")?>">
+              </div>
+            </div>
+          </div>
+          <br>
+  <?php }
+?>
+
+
+    <h5 class="excerpt-post-title">Make Sure To Check Out the Latest Post!</h5>
+
+    <?php
+  
+   // Define our WP Query Parameters
+   $the_query = new WP_Query( 'posts_per_page=3' ); ?>
+     
+     
+     <div class="grid-x bottom-posts align-top">
+   <?php 
+   // Start our WP Query
+   while ($the_query -> have_posts()) : $the_query -> the_post(); 
+   // Display the Post Title with Hyperlink
+   ?>
+
+     <div class="medium-4 cell">
+       <div class="media-object">
+         <div class="media-object-section">
+           <img class="thumbnail" src="<?php echo get_theme_file_uri("/img/placeholder.png")?>">
+         </div>
+         <div class="media-object-section">
+           <h5><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h5>
+
+           <?php 
+           // Display the Post Excerpt
+           the_excerpt(); ?>
+         </div> 
+       </div>
+     </div>
+
+   <?php 
+   // Repeat the process and reset once it hits the limit
+   endwhile;
+   wp_reset_postdata();
+   ?>
+    </div>
+
+    <div class="grid">
+        <div class="grid-x grid-margin-x">
+              <div class="medium-6 cell">
+              <img class="thumbnail image" src="<?php echo get_theme_file_uri("/img/placeholder.png")?>">
+              </div>
+              <div class="medium-5 cell">
+                <br>
+                <h3>Contact Us</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec molestie, ante et luctus vehicula, nisl leo porta lacus, nec laoreet nisi quam a dui. Praesent eu dolor mi. Mauris ultrices condimentum hendrerit.</p>
+                <p>Contact Number: 61+ 1234 5678</p>
+                <p>Email: lego@gmail.com</p>
+                <p>Address: Locked Bag 6, Northbridge WA 6865</p>
+                <hr>
+                <form action="" method='POST'>
+                  <label>Name</label>
+                    <input type="text" placeholder="Name" name='user-name'>
+                  <p><?php if(isset($name_error)) echo $name_error; ?></p>
+
+                  <label>Email </label>
+                    <input type="text" placeholder="Email" name='user-email'>
+                    <p><?php if(isset($email_error)) echo $email_error; ?></p>
+
+                  <label>Message</label>
+                    <textarea placeholder="Message" name='user-message'></textarea>
+                    <p><?php if(isset($message_error)) echo $message_error; ?></p>
+
+                  <input type="submit" class="button expanded" value="Submit" name='user-submit'>
+
+                  <?php 
+        if(isset($_POST['user-submit']) && !isset($name_error) && !isset($subject_error) && !isset($email_error) && !isset($message_error)){
+          $to = 'wilson.gleelover@gmail.com'; 
+          $body = " Name: $name\n E-mail: $email\n Message:\n $message";
+          if(mail($to, $subject, $body)){
+            echo '<p style="color: green">Message sent</p>';
+          }else{
+            echo '<p>Error occurred, please try again later</p>';
+          }
+        }
+      ?>
+                </form>
+                <br>
+              </div>
+            </div>
+        </div>
+<!-- INLUDE CONTACT PAGE HERE -->
+<?php get_footer(); ?>
