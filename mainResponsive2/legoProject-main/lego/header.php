ad3b5d2743b012ca47575d10ac7880c9f8314daf
nikita <88759882+nikita00kumar@users.noreply.github.com>
2021-11-17 13:11:37 +0800

    Add files via upload

---
+++ mainResponsive2/legoProject-main/lego/header.php
@@ -0,0 +1,29 @@
+<html>
+    <head>
+        <?php wp_head() ?>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lego</title>
+    <link rel="stylesheet" href="css/foundation-prototype.css">
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="css/style.css">
+    
+    
+  <meta class="foundation-mq"></head>
+  <body>
+     <a id="home"></a>
+    <div class="top-bar" id="mainNavigation" style="display: flex;">
+      <div class="top-bar-left">
+        <ul class="menu vertical medium-horizontal">
+          <li class="menu-text hide-for-small-only"><a>Logo</a></li>
+        </ul>
+      </div>
+      <div class="top-bar-right">
+        <ul class="menu vertical medium-horizontal">
+          <li class="menu-text hide-for-small-only"><a href="<?php echo site_url(); ?>">Home</a></li>
+          <li class="menu-text hide-for-small-only"><a href=" <?php echo site_url("/blog"); ?>">Post</a></li>
+          <li class="menu-text hide-for-small-only"><a href="<?php echo site_url("/about"); ?>">About</a></li>
+        </ul>
+      </div>
+    </div>
+    <br>
---
+++ mainResponsive2/legoProject-main/template-parts/page/content-front-page.php
@@ -0,0 +1,167 @@
+<?php
+/**
+ * @package WordPress
+ * @subpackage
+ * @since
+ * @version 1.0
+ */
+
+?>
+	
+
+<?php get_header(); ?>
+
+<main>
+  <br>
+      <div class="main-top">
+        <div class="grid-x grid-margin-x">
+        <div class="medium-5 large-4 cell">
+          <h1>Title</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur
+            adipiscing elit. Donec molestie, ante et luctus
+            vehicula, nisl leo porta lacus, nec laoreet nisi
+            quam a dui. Praesent eu dolor mi. Mauris
+            ultrices condimentum hendrerit. Phasellus a
+            tempor nunc, sit amet rhoncus risus. </p>
+        </div>
+        <div class="show-for-small large-4 cell">
+          <img src="<?php echo get_theme_file_uri("assets/img/placeholder.png")?>" alt="">
+        </div>
+        <div class="medium-5 large-4 cell">
+            <h1>Post Title</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur
+            adipiscing elit. Donec molestie, ante et
+            luctus vehicula, nisl leo porta lacus, nec
+            laoreet nisi quam a dui. Praesent eu dolor
+            mi. Mauris ultrices condimentum
+            hendrerit.</p>
+        </div>
+      </div>
+      </div>
+      <br>
+      <div class="hero-section">
+        <div class="hero-section-text">
+          <p>Clickable Link For App Once Available</p>
+        </div>
+      </div>
+    <br>
+    <div class="main-middle">
+    <div class="grid-x grid-margin-x">
+          <div class="medium-6 cell small-order-2 medium-order-1">
+            <img class="thumbnail" src="<?php echo get_theme_file_uri("assets/img/placeholder.png")?>">
+          </div>
+          <div class="medium-6 cell small-order-1 medium-order-2">
+            <h1>Post Title</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur
+            adipiscing elit. Donec molestie, ante et
+            luctus vehicula, nisl leo porta lacus, nec
+            laoreet nisi quam a dui. <a href="home-2/single/">Read More</a>
+            </p>
+          </div>
+        </div>
+      </div>
+      <br>
+
+      
+      <div class="main-bottom">
+      <div class="grid-x align-bottom">
+          <div class="medium-4 cell">
+            <div class="media-object">
+              <div class="media-object-section">
+                <img class="thumbnail" src="<?php echo get_theme_file_uri("assets/img/placeholder.png")?>">
+              </div>
+              <div class="media-object-section">
+                <h3>Post Title</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur
+            adipiscing elit.<a href="home-2/single/">Read More</a>
+              </div>
+            </div>
+            <div class="media-object">
+              <div class="media-object-section">
+                <img class="thumbnail" src="<?php echo get_theme_file_uri("assets/img/placeholder.png")?>">
+              </div>
+              <div class="media-object-section">
+                <h3>Post Title</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur
+            adipiscing elit.<a href="home-2/single/">Read More</a>
+              </div>
+            </div>
+            <div class="media-object">
+              <div class="media-object-section">
+                <img class="thumbnail" src="<?php echo get_theme_file_uri("assets/img/placeholder.png")?>">
+              </div>
+              <div class="media-object-section">
+                <h3>Post Title</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur
+            adipiscing elit.<a href="home-2/single/">Read More</a>
+              </div>
+            </div>
+          </div>
+          <div class="medium-4 cell">
+            <div class="media-object">
+              <div class="media-object-section">
+                <img class="thumbnail" src="<?php echo get_theme_file_uri("assets/img/placeholder.png")?>">
+              </div>
+              <div class="media-object-section">
+                <h3>Post Title</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur
+            adipiscing elit.<a href="home-2/single/">Read More</a>
+              </div>
+            </div>
+            <div class="media-object">
+              <div class="media-object-section">
+                <img class="thumbnail" src="<?php echo get_theme_file_uri("assets/img/placeholder.png")?>">
+              </div>
+              <div class="media-object-section">
+                <h3>Post Title</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur
+            adipiscing elit.<a href="home-2/single/">Read More</a>
+              </div>
+            </div>
+            <div class="media-object">
+              <div class="media-object-section">
+                <img class="thumbnail" src="<?php echo get_theme_file_uri("assets/img/placeholder.png")?>">
+              </div>
+              <div class="media-object-section">
+                <h3>Post Title</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur
+            adipiscing elit.<a href="home-2/single/">Read More</a>
+              </div>
+            </div>
+          </div>
+          <div class="medium-4 cell">
+            <div class="media-object">
+              <div class="media-object-section">
+                <img class="thumbnail" src="<?php echo get_theme_file_uri("assets/img/placeholder.png")?>">
+              </div>
+              <div class="media-object-section">
+                <h3>Post Title</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur
+            adipiscing elit.<a href="home-2/single/">Read More</a>
+              </div>
+            </div>
+            <div class="media-object">
+              <div class="media-object-section">
+                <img class="thumbnail" src="<?php echo get_theme_file_uri("assets/img/placeholder.png")?>">
+              </div>
+              <div class="media-object-section">
+                <h3>Post Title</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur
+            adipiscing elit.<a href="home-2/single/">Read More</a>
+              </div>
+            </div>
+            <div class="media-object">
+              <div class="media-object-section">
+                <img class="thumbnail" src="<?php echo get_theme_file_uri("assets/img/placeholder.png")?>">
+              </div>
+              <div class="media-object-section">
+                <h3>Post Title</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur
+            adipiscing elit.<a href="home-2/single/">Read More</a>
+              </div>
+            </div>
+          </div>
+        </div>      
+      </div>
+      <br>
+    </main>
